84ff6bb19ac8537fc04af4e60743553ff3943fb8
Shawheen Naderi <snaderi@csus.edu>
2022-03-05 11:41:34 -0800

    working implementation, I think, but could be made faster for larger numbers

--- assign_version_3.py
+++ assign_version_3.py
@@ -42,9 +42,34 @@ def assign(A: int, N: int, K: int, verbose=True, **kwargs):
     for i in range(1,N+1):
         line_of_rev_dict["reviewer{0}".format(i)] = []
 
-    #go through each reviewer and give them something from the hat 
-    
+    #go through each reviewer and give them something from the hat
+    #QUESTION:do we need to go further than len(slips)+1? Maybe use a while loop
+    #dog = len(slips)+1
+    for i in range(1,1000000000): #big number
+        i = i%N
+        if(i==0):
+            i = N
+        #print(line_of_rev_dict["reviewer{0}".format(i)])
+        #hand them the slip if they don't have it 
+        for j in range(0,len(slips)):
+            if slips[j] not in line_of_rev_dict["reviewer{0}".format(i)]:
+                line_of_rev_dict = add_values_in_dict(line_of_rev_dict,"reviewer{0}".format(i), [slips[j]])
+                slips.pop(j)
+                break
+
+        if not slips:
+            break
+    return line_of_rev_dict
+
+def add_values_in_dict(sample_dict, key, list_of_values):
+    ''' Append multiple values to a key in 
+        the given dictionary '''
+    if key not in sample_dict:
+        sample_dict[key] = list()
+    sample_dict[key].extend(list_of_values)
+    return sample_dict
 
     
-
+if __name__ == "__main__":
+    print(assign(60, 5, 4))
 
