6338605e1292354b7f3580e010c9821d5d25e72b
Clark Fitzgerald <clarkfitzg@gmail.com>
2022-04-11 14:09:30 -0700

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,13 @@
+from equiassign import *
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        a = assign(19, 4, 2)
+        self.assertEqual(len(a), )
+
+
+if __name__ == '__main__':
+    unittest.main()
